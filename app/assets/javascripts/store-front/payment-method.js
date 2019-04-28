781d83b899bd824ce0b45c878171829cd9cf6188
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:37:36 +0800

    add fields

--- app/assets/javascripts/store-front/payment-method.js
+++ app/assets/javascripts/store-front/payment-method.js
@@ -11,31 +11,37 @@ $(function(){
       },
       error:function(err){
       	console.log(err)
-      }
+        }
     });
 
    $("#continue_to_shipping_method").click(function(){
+       var email = $("#email_address").val()
+       var fname = $("#fname").val();
+       var lname = $("#lname").val();
        var address = $("#address").val();
        var apartment = $("#apartment").val();
        var city = $("#city").val();
        var country = $("#country").val();
        var postal = $("#postal").val();
-       var dataString = "address="+address+"&apartment="+apartment+"&city="+city+"&country="+country+"&postal="+postal+"&is_save_info_checked="+$('#save_this_info').is(":checked");
+       var dataString = "email="+email+"&fname="+fname+"&lname="+lname+"&address="+address+"&apartment="+apartment+"&city="+city+"&country="+country+"&postal="+postal+"&is_save_info_checked="+$('#save_this_info').is(":checked");
        codeAddress();  
-       $.ajax({
-         url:"/continue_to_shipping_method",
-         type:"GET",
-         data:dataString,
-         dataType:"JSON",
-          success:function(data){
-            if(data == "shipping address saved successfuly"){
-              window.location.href = "/checkout_steps/shipping_method";
-            }
-          },
-          error:function(err){
-            console.log(err)
-          }
-       }); 
+             setTimeout(function(){
+               $.ajax({
+                 url:"/continue_to_shipping_method",
+                 type:"GET",
+                 data:dataString,
+                 dataType:"JSON",
+                  success:function(data){
+                    if(data == "shipping address saved successfuly"){
+                      window.location.href = "/checkout_steps/shipping_method";
+                    }
+                  },
+                  error:function(err){
+                    console.log(err)
+                  }
+               });
+             }, 2000);
+               
    });
 
 });
