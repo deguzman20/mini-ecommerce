942dc7f06975cf2d1ae071c4676a3deb0d17c872
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:50:11 +0800

    add migration file for feature information

---
+++ db/migrate/20190423042753_create_feature_infos.rb
@@ -0,0 +1,12 @@
+class CreateFeatureInfos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feature_infos do |t|
+      t.integer :product_id
+      t.string :image
+      t.string :first_text
+      t.string :second_text
+
+      t.timestamps
+    end
+  end
+end
