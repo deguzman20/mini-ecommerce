eb59faf0a0a100c0190aeb52c5b74250e60fcaed
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:41:33 +0800

    add indention

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,40 +3,54 @@ class PagesController < ApplicationController
   end
 
   def product
-   @product       = Product.find(params[:id].to_i)
-   @faqs          = @product.faqs 
-   @tech_specs    = @product.technical_specifications
-   @tect_spec_img = @product.technical_specification_image
-   @charging_capacities = ChargingCapacity.where(product_id:params[:id].to_i)
+    @product       = Product.find(params[:id].to_i)
+    @faqs          = @product.faqs 
+    @tech_specs    = @product.technical_specifications
+    @tect_spec_img = @product.technical_specification_image
+    @charging_capacities = ChargingCapacity.where(product_id:params[:id].to_i)
   end	
-  
+
   def announcement
-   @announcement = Announcement.find(params[:id])
+    @announcement = Announcement.find(params[:id])
   end  
 
   def get_local_storage
-   decoded_hash = JWT.decode(params[:auth_token], Rails.application.secrets.secret_key_base)
-   decoded_hash[0].each { |customer_id| $customer_id = customer_id[1] }
+    decoded_hash = JWT.decode(params[:auth_token], Rails.application.secrets.secret_key_base)
+    decoded_hash[0].each { |customer_id| $customer_id = customer_id[1] }
+  end
+  
+  def order_history
+    @customer_orders = Order.where(customer_id:$customer_id.to_i)
+  end
+
+  def get_product
+    product = Product.find(params[:product_id].to_i)
+    render json: product
+  end
+  
+  def order_product
+    order_product = OrderProduct.where(order_id:params[:id].to_i)
+    render json: order_product
   end
   
   def delete_cart_product
-   cart_product = CartProduct.find(params[:id].to_i)
-   cart_product.delete 
-   get_sub_total
+    cart_product = CartProduct.find(params[:id].to_i)
+    cart_product.delete 
+    get_sub_total
   end
 
   def increase_product_quantity
-   cart_product = CartProduct.find(params[:cart_product_id].to_i)  
-   cart_product.quantity += 1
-   cart_product.save! 
-   get_sub_total
+    cart_product = CartProduct.find(params[:cart_product_id].to_i)  
+    cart_product.quantity += 1
+    cart_product.save! 
+    get_sub_total
   end  
   
   def decrease_product_quantity
-   cart_product = CartProduct.find(params[:cart_product_id].to_i)  
-   cart_product.quantity -= 1
-   cart_product.save!
-   get_sub_total
+    cart_product = CartProduct.find(params[:cart_product_id].to_i)  
+    cart_product.quantity -= 1
+    cart_product.save!
+    get_sub_total
   end
   
   def cart
@@ -45,35 +59,36 @@ class PagesController < ApplicationController
   end
   
   def contact_us 
-   contact = ContactU.new
-   contact.name = params[:name]
-   contact.email_address = params[:email]
-   contact.message = params[:message] 
-   render json: 'Send Successfuly'.to_json if contact.save!
+    contact = ContactU.new
+    contact.name = params[:name]
+    contact.email_address = params[:email]
+    contact.message = params[:message] 
+    render json: 'Send Successfuly'.to_json if contact.save!
   end
 
   def add_to_cart
-	 cart_id = ""
-   product_id = params[:product_id]
-   carts = Cart.where(customer_id: $customer_id.to_i)
-   carts.each do |cart|
-    cart_id = cart.id
-   end	
+	   cart_id = ''
+     product_id = params[:product_id]
+     carts = Cart.where(customer_id: $customer_id.to_i)
+    
+    carts.each do |cart|
+     cart_id = cart.id
+    end	
    
-   if CartProduct.where(cart_id: cart_id.to_i).where(product_id: product_id).present?
-      CartProduct.where(cart_id: cart_id.to_i).where(product_id: product_id).each do |cart_product|
-        cp = CartProduct.find(cart_product.id)
-        cp.quantity += params[:quantity].to_i
-        cp.save! 
-      end   
-   else
-      cart_product = CartProduct.new
-      cart_product.cart_id = cart_id.to_i
-      cart_product.product_id = product_id
-      cart_product.quantity = params[:quantity].to_i
-      cart_product.save!
-   end
-     render json: 'Saved Successful'.to_json
+    if CartProduct.where(cart_id: cart_id.to_i).where(product_id: product_id).present?
+        CartProduct.where(cart_id: cart_id.to_i).where(product_id: product_id).each do |cart_product|
+          cp = CartProduct.find(cart_product.id)
+          cp.quantity += params[:quantity].to_i
+          cp.save! 
+        end   
+    else
+        cart_product = CartProduct.new
+        cart_product.cart_id = cart_id.to_i
+        cart_product.product_id = product_id
+        cart_product.quantity = params[:quantity].to_i
+        cart_product.save!
+    end
+    render json: 'Saved Successful'.to_json
   end
 
   def get_sub_total
@@ -85,11 +100,11 @@ class PagesController < ApplicationController
   end	
   
   def redirect_to_paypal
-   cart_product = CartProduct.new
-   cart = Cart.find_by_customer_id($customer_id.to_i)
-   cart_products = CartProduct.where(cart_id:cart.id)
-   base_url = request.base_url
-   redirect_to cart_product.paypal_url(cart_products ,base_url )
+    cart_product = CartProduct.new
+    cart = Cart.find_by_customer_id($customer_id.to_i)
+    cart_products = CartProduct.where(cart_id:cart.id)
+    base_url = request.base_url
+    redirect_to cart_product.paypal_url(cart_products ,base_url )
   end  
 
   def execute
@@ -104,22 +119,39 @@ class PagesController < ApplicationController
         CartProduct.where(cart_id: cart.id).each { |cart_product| 
                                                     subtotal += cart_product.product.price * cart_product.quantity  }
         transaction_id = JSON.parse(payment.to_json)['transactions'][0]['related_resources'][0]['sale']['id']
-        
+        shipping_address = JSON.parse(payment.to_json)["payer"]["payer_info"]["shipping_address"]
         # create new order
         order = Order.new
         order.total = subtotal
         order.customer_id = $customer_id
         order.payment_order_status_id = 1
         order.paypal_transaction = transaction_id
-        order.Saved!
-         
+        if order.save
+           cart = Cart.find_by_customer_id($customer_id.to_i)
+            CartProduct.where(cart_id:cart.id).each do |cp| 
+               cp_subtotal = 0
+               cp_subtotal += cp.product.price * cp.quantity
+               order_product = OrderProduct.new
+               order_product.order_id = order.id
+               order_product.product_id = cp.product_id 
+               order_product.sub_total = cp_subtotal
+               order_product.save
+            end    
+          
+            customer_shipping_address = CustomerShippingAddress.new
+            customer_shipping_address.customer_id = $customer_id
+            customer_shipping_address.city = shipping_address["city"]
+            customer_shipping_address.postal_code = shipping_address["postal_code"]
+            customer_shipping_address.is_save_info = true
+            customer_shipping_address.save!
+        end
+
         # redirect to the cart page if paypal was success
         redirect_to transactions.href.to_s
     else
       payment.error # Error Hash
     end
   end
-
-
+ 
 end
   
