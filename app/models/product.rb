ecb27b33bf3d94ac6fffe51a644f3c7cca1e5b4b
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:44:25 +0800

    add indention

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,12 @@
 class Product < ApplicationRecord
- has_many :cart_products
- mount_uploader :image, ImageUploader
- has_many :faqs
- has_many :technical_specifications
- has_one :technical_specification_image
- has_many :charging_capacity
+  has_many :cart_products
+  mount_uploader :image, ImageUploader
+  has_many :faqs
+  has_many :technical_specifications
+  has_one :technical_specification_image
+  has_many :charging_capacity
+  has_one :feature_product
+  has_many :feature_infos
+  has_one :product
 end
+	
