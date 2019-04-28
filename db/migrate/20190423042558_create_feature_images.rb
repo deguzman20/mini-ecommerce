1862f7a2615205a7533e3398382251347ffb555a
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:49:48 +0800

    add migration file for feature image

---
+++ db/migrate/20190423042558_create_feature_images.rb
@@ -0,0 +1,10 @@
+class CreateFeatureImages < ActiveRecord::Migration[5.2]
+  def change
+    create_table :feature_images do |t|
+      t.integer :product_id
+      t.string :image
+
+      t.timestamps
+    end
+  end
+end
