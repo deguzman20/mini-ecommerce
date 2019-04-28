5f93f80a471ce722c5e5249b26c9d00f23f672bc
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:39:25 +0800

    add customer_shipping_address_id and add spacing between comma and precision

--- db/migrate/20190415051318_create_orders.rb
+++ db/migrate/20190415051318_create_orders.rb
@@ -1,11 +1,12 @@
 class CreateOrders < ActiveRecord::Migration[5.2]
   def change
     create_table :orders do |t|
+      t.integer :customer_shipping_address_id
       t.integer :customer_id
       t.integer :order_status_id
       t.string  :paypal_transaction
       t.integer :payment_order_status_id	
-      t.string  :total ,precision: 50, scale: 6, default: "0.0" 
+      t.string  :total, precision: 50, scale: 6, default: "0.0" 
       t.timestamps
     end
   end
