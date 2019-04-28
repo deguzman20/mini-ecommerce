acc868daa56a8017ea08786a6f4829e8022a5e2e
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:43:35 +0800

    add association between customer_shipping_address and order_products

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,6 @@ class Order < ApplicationRecord
   belongs_to :customer, optional: true
   belongs_to :order_status, optional: true
   belongs_to :payment_order_status
+  belongs_to :customer_shipping_address
+  has_many :order_products
 end
