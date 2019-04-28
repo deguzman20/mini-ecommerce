626e85f933f5df22faffa90bf3e236fab11a27e4
andy <a.deguzman@jumpdigital.asia>
2019-04-28 15:39:55 +0800

    add email address, firstname and lastname in migration

--- db/migrate/20190414113705_create_customer_shipping_addresses.rb
+++ db/migrate/20190414113705_create_customer_shipping_addresses.rb
@@ -2,6 +2,9 @@ class CreateCustomerShippingAddresses < ActiveRecord::Migration[5.2]
   def change
     create_table :customer_shipping_addresses do |t|
       t.integer :customer_id
+      t.string :email_address
+      t.string :first_name
+      t.string :last_name
       t.text :address
       t.string :apartment
       t.string :city
