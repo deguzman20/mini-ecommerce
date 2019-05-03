2ef7d32da1cdc573cdd4f0aea404f30abcc73984
andy <a.deguzman@jumpdigital.asia>
2019-05-03 09:08:12 +0800

    add seed file

---
+++ db/seeds/12-feature.rb
@@ -0,0 +1,135 @@
+FeatureInfo.create([
+	 {
+	    product_id: 1,
+	    first_text: 'Built-in Flashlight',
+	    second_text: 'Powerful LED Flashlight'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: '12V Lighter Port',
+	 	second_text: '10A (120W total)'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: '6mm Port (output)',
+	 	second_text: '2* 12V, up to 10A (120W Max)'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: 'USB Port (output)',
+	 	second_text: '2* 5V, up to 2.1A (21W Total) Regulated'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: 'LCD Display',
+	 	second_text: 'Real-Time Information LCD Display'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: '220V Power Outlet',
+	 	second_text: '220V AC Inverter 50Hz, 3A 300W, 5002 surge Pure Sine Wave'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: 'Charging Port',
+	 	second_text: '25.2V 4A, 100W max Wall Charger Compatible, Solar Panel Compatible'
+	 },
+	 {
+	 	product_id: 1,
+	 	first_text: 'Durable Splashproof Body',
+	 	second_text: 'Protects the gadget from water splashes'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Input',
+	 	second_text: 'Type C Port'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Output',
+	 	second_text: 'Type C Port'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'AC Output',
+	 	second_text: '220Volts, 85W'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Digital Display',
+	 	second_text: 'Accurate and Precise'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Fan Ventilation',
+	 	second_text: 'Heat Release Function'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Quick Charge 3.0',
+	 	second_text: 'Charge devices up to 4x faster'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Versatile',
+	 	second_text: 'Charge up to 4 devices simultaneously'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Flexible',
+	 	second_text: 'Charge by wall, car or solar charge'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Certified',
+	 	second_text: 'ETL, CE, FCC, RoHS; Safe to take planes'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Accurate',
+	 	second_text: 'LCD power indicator'
+	 },
+	 {
+	 	product_id: 2,
+	 	first_text: 'Ultra Portable',
+	 	second_text: 'Compact and lightweight'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'Over Power Protection',
+	 	second_text: 'Protects device from exceeding its maximum rated capacity'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'Surge Protection',
+	 	second_text: 'Protects device from voltage spikes.'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'Short Circuit Protection',
+	 	second_text: 'Pretects from excessive currents or current beyond the acceptable current rating.'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'Portable',
+	 	second_text: 'Foldable, light and easily transportable'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'USB Port A(output)',
+	 	second_text: '5V, up to 2.4A'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'USB Port C(output)',
+	 	second_text: '5V, up to 3A'
+	 },
+	 {
+	 	product_id: 3,
+	 	first_text: 'FCC & ROHS Certified',
+	 	second_text: 'Federal Communications Commission and Restriction of Hazardous Substances certified!'
+	 }
+
+])
+
+pp 'features created'
