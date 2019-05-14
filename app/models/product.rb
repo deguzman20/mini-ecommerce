1d2e142fdf7a70a35913a21a52ff56eaa7027abf
andy <a.deguzman@jumpdigital.asia>
2019-05-14 15:13:39 +0800

    fix association

--- app/models/product.rb
+++ app/models/product.rb
@@ -7,8 +7,10 @@ class Product < ApplicationRecord
   has_many :technical_specifications
   has_one :technical_specification_image
   has_many :charging_capacity
-  # has_one :feature_product
+  has_many :feature_images
+  has_many :order_products
+  # has_one :feature_produc
   # has_one :product_feature
   has_many :feature_infos
-  has_one :product
+  # has_one :product
 end
