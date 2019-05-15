b791de21048e2ba8a9456827bd47b79b191953f0
andy <a.deguzman@jumpdigital.asia>
2019-05-15 08:02:04 +0800

    add deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,27 +8,27 @@ set :use_sudo, false
 set :stage, :development
 set :deploy_to, "/home/powerlife/public_html/power-life"
 
-# namespace :deploy do
+namespace :deploy do
 
-#   desc "Change HTML Symlink to relative path"
-#   task :create_symlink do
-#     on roles(:app) do
+  desc "Change HTML Symlink to relative path"
+  task :create_symlink do
+    on roles(:app) do
 
-#         #execute "ls -l"
-#         info "Modifying symlink to be relative"
-#         execute "rm -d #{current_path}"
+        #execute "ls -l"
+        info "Modifying symlink to be relative"
+        execute "rm -d #{current_path}"
 
-#         info "Deleted current symlink"
-#         execute "cd /home/powerlife/public_html/power-life && ln -s /home/powerlife/public_html/power-life/releases/#{File.basename release_path} current"
-#         info "Created relative current symlink"
-#     end
-#   end
+        info "Deleted current symlink"
+        execute "cd /home/powerlife/public_html/power-life && ln -s /home/powerlife/public_html/power-life/releases/#{File.basename release_path} current"
+        info "Created relative current symlink"
+    end
+  end
 
-# end
+end
 
 
 append :linked_files, "config/database.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
 
 
-# after :deploy, "deploy:create_symlink"
+after :deploy, "deploy:create_symlink"
